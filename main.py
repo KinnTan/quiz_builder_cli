305880fc0a1bc1010e66f7bec5a8ae1342c8a4f9
Kinn Tan <tankinnr@gmail.com>
2025-04-07 15:33:35 +0800

    Set Variables  - Input for the question, choice a to d and choosing correct choice

---
+++ main.py
@@ -0,0 +1,9 @@
+question = input("Enter the question you want to add: ")
+
+choice_a = input("Enter a choice for a: ")
+choice_b = input("Enter a choice for b: ")
+choice_c = input("Enter a choice for c: ")
+choice_d = input("Enter a choice for d: ")
+
+correct_choice = input("Enter the correct choice: ")
+
