4ee655eaf9260877086d3f787da6e979891bd3aa
Kinn Tan <tankinnr@gmail.com>
2025-04-07 15:50:08 +0800

    Add while loops - retry getting input for correct_choice when it is invalid

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-question = input("Enter the question you want to add: ")
 
-choice_a = input("Enter a choice for a: ").lower()
-choice_b = input("Enter a choice for b: ").lower()
-choice_c = input("Enter a choice for c: ").lower()
-choice_d = input("Enter a choice for d: ").lower()
+while True:
+    question = input("Enter the question you want to add: ")
 
+    choice_a = input("Enter a choice for a: ").lower()
+    choice_b = input("Enter a choice for b: ").lower()
+    choice_c = input("Enter a choice for c: ").lower()
+    choice_d = input("Enter a choice for d: ").lower()
 
-correct_choice = input("Enter the correct choice: "). lower()
-
+    while True:
+        correct_choice = input("Enter the correct choice: "). lower()
+        if correct_choice not in ["a", "b", "c", "d"]:
+            print("Invalid choice")
+        else:
+            break
 
