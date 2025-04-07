01308365ae8983bdb1f96c96e4efc0184499aecd
Kinn Tan <tankinnr@gmail.com>
2025-04-07 15:36:13 +0800

    Lowercase choice_a to d and correct_choice

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 question = input("Enter the question you want to add: ")
 
-choice_a = input("Enter a choice for a: ")
-choice_b = input("Enter a choice for b: ")
-choice_c = input("Enter a choice for c: ")
-choice_d = input("Enter a choice for d: ")
+choice_a = input("Enter a choice for a: ").lower()
+choice_b = input("Enter a choice for b: ").lower()
+choice_c = input("Enter a choice for c: ").lower()
+choice_d = input("Enter a choice for d: ").lower()
 
-correct_choice = input("Enter the correct choice: ")
+
+correct_choice = input("Enter the correct choice: "). lower()
+
 
